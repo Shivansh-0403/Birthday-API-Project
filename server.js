38141d791854a56d1ec633131cf7a7caa9aa2456
Shivansh Srivastava <113630031+Shivansh-0403@users.noreply.github.com>
2024-01-14 21:11:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.get('/', (req, res) => {
 })
 
 app.post('/message', async (req, res) => {
-
     try {
         const result = await axios.get("https://api.api-ninjas.com/v1/quotes?category=birthday", {
             headers: {
@@ -22,37 +21,18 @@ app.post('/message', async (req, res) => {
             }
         })
 
-        // const result = await axios.get(API_URL + "/secrets/" + searchId, config);
         console.log(result.data[0].quote);
         res.render("index.ejs", { content: result.data[0].quote });
 
     } catch (error) {
         res.render("index.ejs", { content: JSON.stringify(error.response.data) });
     }
-
-    //   res.json({ message: 'Post request received successfully!' });
-
 });
 
 app.post("/clear", async(req, res) => {
-    
     res.redirect("/");
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-    // try {
-    //     const result = await axios.get("https://api.api-ninjas.com/v1/quotes?category=birthday", {
-    //         headers: {
-    //             'X-Api-Key': 'nIXqkxGINBkbIv3o1N3fiQ==97fIZV1vlr0dQ3yJ'
-    //         }
-    //     })
-
-    //     // const result = await axios.get(API_URL + "/secrets/" + searchId, config);
-    //     console.log(result.data[0].quote);
-    //     res.render("index.ejs", { content: result.data[0].quote });
-
-    // } catch (error) {
-    //     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-    // }
